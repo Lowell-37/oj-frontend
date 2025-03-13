155ad9bb0d246b90af44ca0487f955904096bc03
Lowell-37 <a2484822206@126.com>
2025-03-13 11:04:26 +0800

    添加登录页面、权限校验功能

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,27 +1,93 @@
 <template>
-  <div id="globalHeader">
-    <a-menu mode="horizontal" :default-selected-keys="['1']">
-      <a-menu-item
-        key="0"
-        :style="{ padding: 0, marginRight: '38px' }"
-        disabled
+  <a-row id="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
       >
-        <div class="tittle-bar">
-          <img class="logo" src="../assets/oj-logo.svg" />
-        </div>
-      </a-menu-item>
-      <a-menu-item key="1">Home</a-menu-item>
-      <a-menu-item key="2">Solution</a-menu-item>
-      <a-menu-item key="3">Cloud Service</a-menu-item>
-      <a-menu-item key="4">Cooperation</a-menu-item>
-    </a-menu>
-  </div>
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/oj-logo.svg" />
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        {{ store.state.user?.loginUser?.userName ?? "未登录" }}
+      </div>
+    </a-col>
+  </a-row>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+import checkAccess from "@/access/checkAccess";
+import ACCESS_ENUM from "@/access/accessEnum";
+
+const router = useRouter();
+const store = useStore();
+
+// 展示在菜单的路由数组
+const visibleRoutes = computed(() => {
+  return routes.filter((item, index) => {
+    if (item.meta?.hideInMenu) {
+      return false;
+    }
+    // 根据权限过滤菜单
+    if (
+      !checkAccess(store.state.user.loginUser, item?.meta?.access as string)
+    ) {
+      return false;
+    }
+    return true;
+  });
+});
+
+// 默认主页
+const selectedKeys = ref(["/"]);
+
+// 路由跳转后，更新选中的菜单项
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+console.log();
+
+setTimeout(() => {
+  store.dispatch("user/getLoginUser", {
+    userName: "管理员",
+    userRole: ACCESS_ENUM.ADMIN,
+  });
+}, 3000);
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
 
 <style scoped>
-.tittle-bar {
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
 }
 
 .logo {
--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="basicLayout">
-    <a-layout style="height: 400px">
+    <a-layout style="min-height: 100vh">
       <a-layout-header class="header">
         <GlobalHeader />
       </a-layout-header>
       <a-layout-content class="content">
         <router-view />
       </a-layout-content>
-
       <a-layout-footer class="footer">
-        <a href="https://github.com/Lowell-37">Created By Lowell</a>
+        <a href="https://github.com/Lowell-37" target="_blank">
+          Created By Lowell
+        </a>
       </a-layout-footer>
     </a-layout>
   </div>
@@ -20,24 +21,30 @@
 }
 
 #basicLayout .header {
-  background: red;
   margin-bottom: 16px;
+  box-shadow: #eee 1px 1px 5px;
 }
 
 #basicLayout .content {
   background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+  padding: 20px;
 }
 
 #basicLayout .footer {
-  background: aquamarine;
+  background: #efefef;
   padding: 16px;
-  position: absolute;
+  position: sticky;
   bottom: 0;
   left: 0;
   right: 0;
   text-align: center;
 }
 </style>
-<script setup lang="ts">
-import GlobalHeader from "@/components/GlobalHeader.vue";
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
 </script>
